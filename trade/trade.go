0f81a2fb6459647d565a7cc59a4c16c2ada50dc0
modood <modood@qq.com>
2018-01-24 00:37:26 +0800

    feat: add func Position
    Position return my open interest status

--- trade/trade.go
+++ trade/trade.go
@@ -1,1 +1,37 @@
 package trade
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/modood/cts/gateio"
+)
+
+const (
+	None  = -1 // 未知
+	Empty = 0  // 空仓
+	Full  = 1  // 满仓
+)
+
+// Position return my open interest status
+func Position() (int8, error) {
+	b, err := gateio.MyBalance()
+	if err != nil {
+		return None, err
+	}
+
+	usdt, ok := b.Available["USDT"]
+	if !ok {
+		return None, errors.New("Have no USDT")
+	}
+
+	f, err := strconv.ParseFloat(usdt, 64)
+	if err != nil {
+		return None, err
+	}
+
+	if f > 10 {
+		return Empty, nil
+	}
+	return Full, nil
+}
--- trade/trade_test.go
+++ trade/trade_test.go
@@ -1,1 +1,16 @@
 package trade
+
+import (
+	"testing"
+
+	"github.com/modood/cts/gateio"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestPosition(t *testing.T) {
+	Convey("should get position unsuccessfully", t, func(c C) {
+		gateio.Init("key", "secret")
+		_, err := Position()
+		So(err, ShouldNotBeNil)
+	})
+}
