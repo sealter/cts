e32eacdc403907fff8bac007bc7089ec3c8fec6e
modood <modood@qq.com>
2018-01-24 01:52:05 +0800

    feat: add func AllOut and Flush

--- trade/trade.go
+++ trade/trade.go
@@ -2,6 +2,7 @@ package trade
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"strconv"
 
@@ -14,38 +15,46 @@ const (
 	Full  = 1  // 满仓
 )
 
-// Position return my open interest status
-func Position() (position int8, balance float64, e error) {
+var balance *gateio.Balance
+
+// Flush refresh balance cache
+func Flush() error {
 	b, err := gateio.MyBalance()
-	if err != nil {
-		return None, 0, err
-	}
-
-	usdt, ok := b.Available["USDT"]
-	if !ok {
-		return None, 0, errors.New("Have no USDT")
-	}
-
-	f, err := strconv.ParseFloat(usdt, 64)
-	if err != nil {
-		return None, 0, err
-	}
-
-	if f > 10 {
-		return Empty, f, nil
-	}
-	return Full, f, nil
-}
-
-// Allin all in
-func AllIn(currency string) error {
-	p, f, err := Position()
 	if err != nil {
 		return err
 	}
 
+	balance = b
+
+	return nil
+}
+
+// Position return my open interest status
+func Position() (int8, error) {
+	usdt, err := carry("USDT")
+	if err != nil {
+		return None, err
+	}
+
+	if usdt > 10 {
+		return Empty, nil
+	}
+	return Full, nil
+}
+
+// Allin all in
+func AllIn(currency string) error {
+	p, err := Position()
+	if err != nil {
+		return err
+	}
 	if p == Full {
 		return nil
+	}
+
+	usdt, err := carry("USDT")
+	if err != nil {
+		return err
 	}
 
 	pair, err := gateio.Ticker(currency)
@@ -53,10 +62,58 @@ func AllIn(currency string) error {
 		return err
 	}
 
-	amount := (f - 1) / pair.Last
+	amount := (usdt - 1) / pair.Last
 
 	log.Println("buying", currency, pair.Last, amount)
 	// _, err := Buy(currency, pair.Last, amount)
 
 	return err
+}
+
+// AllOut all out
+func AllOut(currency, coin string) error {
+	p, err := Position()
+	if err != nil {
+		return err
+	}
+	if p == Empty {
+		return nil
+	}
+
+	amount, err := carry(coin)
+	if err != nil {
+		return err
+	}
+
+	pair, err := gateio.Ticker(currency)
+	if err != nil {
+		return err
+	}
+
+	log.Println("selling", currency, pair.Last, amount)
+	// _, err := Sell(currency, pair.Last, amount)
+
+	return err
+}
+
+func carry(coin string) (float64, error) {
+	if balance == nil {
+		err := Flush()
+
+		if err != nil {
+			return None, err
+		}
+	}
+
+	str, ok := balance.Available[coin]
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("have no %s", coin))
+	}
+
+	f, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return f, nil
 }
--- trade/trade_test.go
+++ trade/trade_test.go
@@ -7,10 +7,18 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func TestFlush(t *testing.T) {
+	Convey("should refresh balance cache unsuccessfully", t, func(c C) {
+		gateio.Init("key", "secret")
+		err := Flush()
+		So(err, ShouldNotBeNil)
+	})
+}
+
 func TestPosition(t *testing.T) {
 	Convey("should get position unsuccessfully", t, func(c C) {
 		gateio.Init("key", "secret")
-		_, _, err := Position()
+		_, err := Position()
 		So(err, ShouldNotBeNil)
 	})
 }
@@ -19,6 +27,14 @@ func TestAllIn(t *testing.T) {
 	Convey("should all in unsuccessfully", t, func(c C) {
 		gateio.Init("key", "secret")
 		err := AllIn("btc_usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestAllOut(t *testing.T) {
+	Convey("should all out unsuccessfully", t, func(c C) {
+		gateio.Init("key", "secret")
+		err := AllOut("btc_usdt", "BTC")
 		So(err, ShouldNotBeNil)
 	})
 }
