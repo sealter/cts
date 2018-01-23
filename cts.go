5cbee2d0a327311848f3ec6dcf8e7f15542d18e3
modood <modood@qq.com>
2018-01-24 02:06:47 +0800

    feat: basic running

--- cts.go
+++ cts.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/modood/cts/gateio"
+	"github.com/modood/cts/strategy"
+	"github.com/modood/cts/trade"
 	"github.com/urfave/cli"
 )
 
@@ -27,18 +28,40 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) error {
+		gateio.Init("apikey", "secretkey")
 		for {
-			// TODO
-			btc, err := gateio.Ticker("btc_usdt")
+			time.Sleep(time.Second * 1)
+
+			err := trade.Flush()
 			if err != nil {
-				return err
+				log.Println(err)
+				continue
 			}
 
-			log.Println(
-				fmt.Sprintf("BTC/USDT $%f, 24h Change: %f%%",
-					btc.Last, btc.PercentChange))
+			sig, err := strategy.RippleDoge()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
 
-			time.Sleep(time.Second * 10)
+			switch sig {
+			case strategy.SIG_RISE:
+				err := trade.AllIn("btc_usdt")
+				if err != nil {
+					log.Println(err)
+					break
+				}
+			case strategy.SIG_FALL:
+				err := trade.AllOut("btc_usdt", "BTC")
+				if err != nil {
+					log.Println(err)
+					break
+				}
+			case strategy.SIG_NONE:
+				fallthrough
+			default:
+				log.Println("do nothing")
+			}
 		}
 		return nil
 	}
