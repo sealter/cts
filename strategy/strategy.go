7a9f8c0b0ce53693d7d03122dc47877b171001de
modood <modood@qq.com>
2018-01-23 10:12:20 +0800

    feat: add a simple strategy refers to ripple and doge

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -1,1 +1,33 @@
 package strategy
+
+import (
+	"github.com/modood/cts/gateio"
+)
+
+const (
+	SIG_NONE = iota // none
+	SIG_RISE        // going up
+	SIG_FALL        // going down
+)
+
+// RippleDoge is a simple strategy refers to ripple and doge
+func RippleDoge() (uint8, error) {
+	doge, err := gateio.Ticker("doge_usdt")
+	if err != nil {
+		return SIG_NONE, err
+	}
+
+	xrp, err := gateio.Ticker("xrp_usdt")
+	if err != nil {
+		return SIG_NONE, err
+	}
+
+	if doge.PercentChange > 5 && xrp.PercentChange > 5 {
+		return SIG_RISE, nil
+	}
+	if doge.PercentChange < -5 && xrp.PercentChange < -5 {
+		return SIG_FALL, nil
+	}
+
+	return SIG_NONE, nil
+}
--- strategy/strategy_test.go
+++ strategy/strategy_test.go
@@ -1,1 +1,22 @@
 package strategy
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestRippleDoge(t *testing.T) {
+	Convey("should check ripple and doge successfully", t, func(c C) {
+		ch := make(chan uint8)
+
+		go func() {
+			sig, err := RippleDoge()
+			c.So(err, ShouldBeNil)
+			ch <- sig
+			close(ch)
+		}()
+
+		c.So([]uint8{SIG_NONE, SIG_RISE, SIG_FALL}, ShouldContain, <-ch)
+	})
+}
