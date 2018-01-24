5742358c63c173e2abf7fe5c6ae9ffd279a7f70b
modood <modood@qq.com>
2018-01-24 15:55:05 +0800

    feat: add main logic

--- cts.go
+++ cts.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/modood/cts/gateio"
@@ -19,6 +20,10 @@ func main() {
 	app.Author = "modood - https://github.com/modood"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
+			Name:  "currency",
+			Usage: "currency name, base currency should be usdt(e.g., btc_usdt, xrp_usdt, etc.)",
+		},
+		cli.StringFlag{
 			Name:  "key",
 			Usage: "your api key",
 		},
@@ -27,44 +32,55 @@ func main() {
 			Usage: "your secret key",
 		},
 	}
-	app.Action = func(c *cli.Context) error {
-		gateio.Init("apikey", "secretkey")
-		for {
-			time.Sleep(time.Second * 1)
-
-			err := trade.Flush()
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-
-			sig, err := strategy.RippleDoge()
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-
-			switch sig {
-			case strategy.SIG_RISE:
-				err := trade.AllIn("btc_usdt")
-				if err != nil {
-					log.Println(err)
-					break
-				}
-			case strategy.SIG_FALL:
-				err := trade.AllOut("btc_usdt", "BTC")
-				if err != nil {
-					log.Println(err)
-					break
-				}
-			case strategy.SIG_NONE:
-				fallthrough
-			default:
-				log.Println("do nothing")
-			}
-		}
-		return nil
-	}
-
+	app.Action = action
 	app.Run(os.Args)
+}
+
+func action(c *cli.Context) error {
+	log.Println("starting...")
+	gateio.Init(c.String("key"), c.String("secret"))
+
+	for {
+		time.Sleep(time.Second * 1)
+
+		err := trade.Flush()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		sig, err := strategy.RippleDoge()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		err = exec(sig, c.String("currency"))
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+	return nil
+}
+
+func exec(signal uint8, currency string) error {
+	switch signal {
+	case strategy.SIG_RISE:
+		err := trade.AllIn(currency)
+		if err != nil {
+			return err
+		}
+	case strategy.SIG_FALL:
+		coin := strings.ToUpper(strings.Split(currency, "_")[0])
+		err := trade.AllOut(currency, coin)
+		if err != nil {
+			return err
+		}
+	case strategy.SIG_NONE:
+		fallthrough
+	default:
+		log.Println("do nothing")
+	}
+	return nil
 }
--- cts_test.go
+++ cts_test.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/modood/cts/gateio"
+	"github.com/modood/cts/strategy"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFlush(t *testing.T) {
+	Convey("should refresh balance cache unsuccessfully", t, func(c C) {
+		gateio.Init("apikey", "secretkey")
+		var err error
+		err = exec(strategy.SIG_RISE, "doge_usdt")
+		So(err, ShouldNotBeNil)
+
+		err = exec(strategy.SIG_FALL, "doge_usdt")
+		So(err, ShouldNotBeNil)
+
+		err = exec(strategy.SIG_NONE, "doge_usdt")
+		So(err, ShouldBeNil)
+	})
+}
