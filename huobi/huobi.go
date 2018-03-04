e2e9bdf9f7bef16aa81babd0cf078a59d12cfef0
modood <modood@qq.com>
2018-03-04 22:27:16 +0800

    feat: add huobi api

--- huobi/huobi.go
+++ huobi/huobi.go
@@ -10,9 +10,11 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"net/url"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -24,17 +26,69 @@ import (
 type (
 	// Account ...
 	Account struct {
-		ID       uint32
+		ID       uint64
 		Type     string
 		State    string
-		Balances []Available `mapstructure:"list" json:"list"`
+		Symbol   string
+		FlPrice  float64 `mapstructure:"fl-price" json:"fl-price"`
+		FlType   string  `mapstructure:"fl-type" json:"fl-type"`
+		RiskRate float64 `mapstructure:"risk-rate" json:"risk-rate"`
+		List     []struct {
+			Currency string
+			Type     string
+			Balance  float64
+		}
 	}
 
-	// Available ...
-	Available struct {
-		Currency string
-		Type     string // 可用
-		Balance  string // 已锁定
+	// OpenOrder ...
+	OpenOrder struct {
+		ID              uint64
+		AccountID       uint64 `mapstructure:"account-id" json:"account-id"`
+		Source          string
+		Type            string
+		State           string
+		Symbol          string
+		Amount          float64
+		Price           float64
+		FieldAmount     float64 `mapstructure:"field-amount" json:"field-amount"`
+		FieldCashAmount float64 `mapstructure:"field-cash-amount" json:"field-cash-amount"`
+		FieldFees       float64 `mapstructure:"field-fees" json:"field-fees"`
+		CreatedAt       uint64  `mapstructure:"created-at" json:"created-at"`
+		FinishedAt      uint64  `mapstructure:"finished-at" json:"finished-at"`
+		CanceledAt      uint64  `mapstructure:"canceled-at" json:"canceled-at"`
+	}
+
+	// Order ...
+	Order struct {
+		ID        uint64
+		OrderID   uint64 `mapstructure:"order-id" json:"order-id"`
+		MatchID   uint64 `mapstructure:"match-id" json:"match-id"`
+		Source    string
+		Type      string
+		Symbol    string
+		Price     float64
+		Amount    float64 `mapstructure:"filled-amount" json:"filled-amount"`
+		Fees      float64 `mapstructure:"filled-fees" json:"filled-fees"`
+		Points    float64 `mapstructure:"filled-points" json:"filled-points"`
+		CreatedAt uint64  `mapstructure:"created-at" json:"created-at"`
+	}
+
+	// BorrowOrder ...
+	BorrowOrder struct {
+		ID              uint64
+		State           string
+		UserID          uint64 `mapstructure:"user-id" json:"user-id"`
+		AccountID       uint64 `mapstructure:"account-id" json:"account-id"`
+		Symbol          string
+		Currency        string
+		LoanAmount      float64 `mapstructure:"loan-amount" json:"loan-amount"`
+		LoanBalance     float64 `mapstructure:"loan-balance" json:"loan-balance"`
+		InterestAmount  float64 `mapstructure:"interest-amount" json:"interest-amount"`
+		InterestBalance float64 `mapstructure:"interest-balance" json:"interest-balance"`
+		InterestRate    float64 `mapstructure:"interest-rate" json:"interest-rate"`
+		CreatedAt       uint64  `mapstructure:"created-at" json:"created-at"`
+		UpdatedAt       uint64  `mapstructure:"updated-at" json:"updated-at"`
+		AccruedAt       uint64  `mapstructure:"accrued-at" json:"accrued-at"`
 	}
 
 	huobiError struct {
@@ -112,6 +166,224 @@ func MarginAccount(currency string) (*Account, error) {
 	}
 
 	return &r.Data[0], nil
+}
+
+// Orders return finished orders
+func Orders(currency string) ([]Order, error) {
+	bs, err := req("GET", "https://api.huobi.pro/v1/order/matchresults",
+		map[string]string{
+			"symbol": hCurrency(currency),
+		})
+	if err := handle(bs, err); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bs, &m)
+	if err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	t := struct {
+		Orders []Order `mapstructure:"data"`
+	}{}
+	if err := util.Decode(m, &t); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	return t.Orders, nil
+}
+
+// OpenOrders return pendding orders
+func OpenOrders(currency string) ([]OpenOrder, error) {
+	bs, err := req("GET", "https://api.huobi.pro/v1/order/orders",
+		map[string]string{
+			"symbol": hCurrency(currency),
+			"states": "pre-submitted,submitted,partial-filled",
+		})
+	if err := handle(bs, err); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bs, &m)
+	if err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	t := struct {
+		Orders []OpenOrder `mapstructure:"data"`
+	}{}
+	if err := util.Decode(m, &t); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	return t.Orders, nil
+}
+
+// BorrowOrders return borrow orders
+func BorrowOrders(currency string) ([]BorrowOrder, error) {
+	bs, err := req("GET", "https://api.huobi.pro/v1/margin/loan-orders",
+		map[string]string{
+			"symbol": hCurrency(currency),
+		})
+	if err := handle(bs, err); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bs, &m)
+	if err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	t := struct {
+		Orders []BorrowOrder `mapstructure:"data"`
+	}{}
+	if err := util.Decode(m, &t); err != nil {
+		return nil, errors.Wrap(err, util.FuncName())
+	}
+
+	return t.Orders, nil
+}
+
+// MarginTrade place new margin order
+func MarginTrade(cmd, currency string, amount float64) (uint64, error) {
+	a, err := MarginAccount(currency)
+	if err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	params := map[string]string{
+		"account-id": strconv.FormatUint(a.ID, 10),
+		"amount":     strconv.FormatFloat(amount, 'f', -1, 64),
+		"source":     "margin-api",
+		"symbol":     hCurrency(currency),
+	}
+
+	switch cmd {
+	case "BUY":
+		params["type"] = "buy-market"
+	case "SELL":
+		params["type"] = "sell-market"
+	case "TEST":
+		params["price"] = "100"
+		params["type"] = "buy-limit"
+	default:
+		return 0, errors.Wrap(errors.New("unknown trade type"), util.FuncName())
+	}
+
+	bs, err := req("POST", "https://api.huobi.pro/v1/order/orders/place", params)
+	if err := handle(bs, err); err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bs, &m)
+	if err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	t := struct {
+		ID string `mapstructure:"data"`
+	}{}
+	if err := util.Decode(m, &t); err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	return strconv.ParseUint(t.ID, 10, 64)
+}
+
+// BorrowAvailable return available amount to borrow
+func BorrowAvailable(currency, symbol string) (float64, error) {
+	a, err := MarginAccount(currency)
+	if err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+	for _, v := range a.List {
+		if v.Type == "loan-available" && v.Currency == symbol {
+			return v.Balance, nil
+		}
+	}
+	return 0, nil
+}
+
+// Borrow borrow money
+func Borrow(currency, symbol string, amount float64) (uint64, error) {
+	params := map[string]string{
+		"symbol":   hCurrency(currency),
+		"currency": symbol, // lol, funny ^_^. currency => symbol, symbol => currency
+		"amount":   floor(amount, 3),
+	}
+
+	bs, err := req("POST", "https://api.huobi.pro/v1/margin/orders", params)
+	if err := handle(bs, err); err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	m := make(map[string]interface{})
+	err = json.Unmarshal(bs, &m)
+	if err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	t := struct {
+		ID string `mapstructure:"data"`
+	}{}
+	if err := util.Decode(m, &t); err != nil {
+		return 0, errors.Wrap(err, util.FuncName())
+	}
+
+	return strconv.ParseUint(t.ID, 10, 64)
+}
+
+// Repay all debt
+func Repay(currency, symbol string) error {
+	bos, err := BorrowOrders(currency)
+	if err != nil {
+		return errors.Wrap(err, util.FuncName())
+	}
+
+	var errs []string
+	for _, v := range bos {
+		if v.Currency != symbol || v.State != "accrual" {
+			continue
+		}
+		bs, err := req("POST", "https://api.huobi.pro/v1/margin/orders/"+strconv.FormatUint(v.ID, 10)+"/repay",
+			map[string]string{
+				"amount": floor(v.LoanAmount+v.InterestAmount, 8),
+			})
+		if err := handle(bs, err); err != nil {
+			errs = append(errs, err.Error()+"(ID: "+strconv.FormatUint(v.ID, 10)+")")
+			continue
+		}
+	}
+	if len(errs) != 0 {
+		return errors.Wrap(errors.New(strings.Join(errs, ";")), util.FuncName())
+	}
+	return nil
+}
+
+// CancelAll cancel all open orders
+func CancelAll(currency string) error {
+	oos, err := OpenOrders(currency)
+	if err != nil {
+		return errors.Wrap(err, util.FuncName())
+	}
+
+	var errs []string
+	for _, v := range oos {
+		bs, err := req("POST", "https://api.huobi.pro/v1/order/orders/"+
+			strconv.FormatUint(v.ID, 10)+"/submitcancel", map[string]string{})
+		if err := handle(bs, err); err != nil {
+			errs = append(errs, err.Error()+"(ID: "+strconv.FormatUint(v.ID, 10)+")")
+			continue
+		}
+	}
+	if len(errs) != 0 {
+		return errors.Wrap(errors.New(strings.Join(errs, ";")), util.FuncName())
+	}
+	return nil
 }
 
 func sign(content string) (string, error) {
@@ -211,6 +483,11 @@ func querystring(m map[string]string) string {
 
 func hCurrency(currency string) string {
 	return strings.Replace(currency, "_", "", -1)
+}
+
+func floor(f float64, prec int) string {
+	i := math.Pow10(prec)
+	return strconv.FormatFloat(math.Floor(f*i)/i, 'f', -1, 64)
 }
 
 func handle(bs []byte, err error) error {
--- huobi/huobi_test.go
+++ huobi/huobi_test.go
@@ -19,21 +19,78 @@ func TestInit(t *testing.T) {
 func TestMarginAccount(t *testing.T) {
 	Convey("should return margin account unsuccessfully", t, func() {
 		Init("apikey", "secretkey")
-		_, err := MarginAccount("btcusdt")
+		_, err := MarginAccount("btc_usdt")
 		So(err, ShouldNotBeNil)
 	})
 }
 
-func TestReq(t *testing.T) {
-	Convey("should transfer unsuccessfully", t, func() {
+func TestOrders(t *testing.T) {
+	Convey("should return finished orders unsuccessfully", t, func() {
 		Init("apikey", "secretkey")
-		bs, err := req("POST", "https://api.huobi.pro/v1/dw/transfer-out/margin", map[string]string{
-			"symbol":   "btcusdt",
-			"currency": "usdt",
-			"amount":   "1",
-		})
-		So(err, ShouldBeNil)
-		err = handle(bs, err)
+		_, err := Orders("btc_usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestOpenOrders(t *testing.T) {
+	Convey("should return pendding orders unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		_, err := OpenOrders("btc_usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestBorrowOrders(t *testing.T) {
+	Convey("should return borrow orders unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		_, err := BorrowOrders("btc_usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestMarginTrade(t *testing.T) {
+	Convey("should trade unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		_, err := MarginTrade("TEST", "btc_usdt", 1)
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestBorrowAvailable(t *testing.T) {
+	Convey("should return available amount unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		_, err := BorrowAvailable("btc_usdt", "usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestBorrow(t *testing.T) {
+	Convey("should return borrow unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		currency := "btc_usdt"
+		symbol := "usdt"
+
+		amount, err := BorrowAvailable(currency, symbol)
+		So(err, ShouldNotBeNil)
+		if amount != 0 {
+			_, err := Borrow(currency, symbol, amount)
+			So(err, ShouldBeNil)
+		}
+	})
+}
+
+func TestRepay(t *testing.T) {
+	Convey("should return repay all debt unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		err := Repay("btc_usdt", "usdt")
+		So(err, ShouldNotBeNil)
+	})
+}
+
+func TestCancelAll(t *testing.T) {
+	Convey("should return cancel all pendding orders unsuccessfully", t, func() {
+		Init("apikey", "secretkey")
+		err := CancelAll("btc_usdt")
 		So(err, ShouldNotBeNil)
 	})
 }
